fed5b15eb2e2d1acca169e38ad0975cda78e6f99
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-15 12:22:19 +0600

    added nav logo and inline style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import { navbarStyles } from "../../assets/dummyStyles";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
     return (
-        <div className="text-4xl">
-            hello
-        </div>
+        <>
+            <nav className={`${navbarStyles.nav} ${scrolled ? navbarStyles.scrolledNav : navbarStyles.unscrolledNav}`}>
+
+                <div className={navbarStyles.borderGradient}>
+                    <div className={navbarStyles.particlesContainer}></div>
+                    <div
+                        className={`${navbarStyles.particle} w-24 h-24 bg-emerald-500/5 -top-12 left-1/4 ${navbarStyles.floatAnimation}`}
+                    />
+                    <div
+                        className={`${navbarStyles.particle} w-32 h-32 bg-green-500/5 -bottom-16 left-2/3 ${navbarStyles.floatSlowAnimation}`}
+                    />
+                    <div
+                        className={`${navbarStyles.particle} w-16 h-16 bg-teal-500/5 -top-8 left-3/4 ${navbarStyles.floatSlowerAnimation}`}
+                    />
+                </div>
+
+                {/* Nav logo  */}
+                <div className={navbarStyles.container}>
+                    <div className={navbarStyles.innerContainer}>
+                        <NavLink to='/' className={navbarStyles.logoLink}>
+                            <img
+                                src="/src/assets/favicon.svg"
+                                alt="G-Mart Logo"
+                                className="w-9 h-9 object-contain drop-shadow-md"
+                            />
+                            <h1 className="text-2xl sm:text-3xl font-extrabold tracking-wide bg-gradient-to-r from-cyan-300 via-cyan-600 to-teal-500 bg-clip-text text-transparent">
+                                Mart.
+                                <span className="text-sm font-semibold text-gray-300">BD</span>
+                            </h1>
+                        </NavLink>
+                    </div>
+                </div>
+
+            </nav>
+        </>
     );
 };
 

30f7d4cd4eb808022f48ee1fd1d539a2ffee8414
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-17 22:58:11 +0600

    added hero section and hero ui

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,9 +1,13 @@
+import Hero from '../HeroSection/Hero';
 import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <div className='max-w-7xl mx-auto pt-10'>
+                <Hero></Hero>
+            </div>
         </div>
     );
 };

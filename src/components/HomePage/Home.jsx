4304df1fa086269ce73cf22a3f043cbf257e9fc2
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-17 23:04:42 +0600

    updated main layout

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,13 +1,9 @@
-import Hero from '../HeroSection/Hero';
-import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
+
     return (
-        <div>
-            <Navbar></Navbar>
-            <div className='max-w-7xl mx-auto pt-10'>
-                <Hero></Hero>
-            </div>
+        <div className="">
+
         </div>
     );
 };
--- src/mainLayout/MainLayOut.jsx
+++ src/mainLayout/MainLayOut.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Hero from "../components/HeroSection/Hero";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayOut = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet />
+            <div className='max-w-7xl mx-auto mt-10'>
+                <Hero></Hero>
+            </div>
         </div>
     );
 };

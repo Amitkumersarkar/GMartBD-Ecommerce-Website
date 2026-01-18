b079e8df86bf73a7a35d8f11f0189d2f250ba7a1
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-18 23:46:38 +0600

    added product card

--- src/pages/BestSeller/BestSeller.jsx
+++ src/pages/BestSeller/BestSeller.jsx
@@ -3,7 +3,7 @@ const BestSeller = () => {
 
     return (
         <div className="mt-16">
-            <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
+            <p className="text-2xl text-gray-700 md:text-3xl font-medium">Best Sellers</p>
             <div className="">
 
             </div>

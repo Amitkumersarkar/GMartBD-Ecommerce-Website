55b4c9b741059af8d23e3eaa90e8f721929a6400
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-18 22:14:37 +0600

    added categries sections

---
+++ src/pages/Categories/Categories.jsx
@@ -0,0 +1,18 @@
+import { assets } from "../../assets/assets";
+
+const Categories = () => {
+
+    return (
+        <div>
+            <p className="">Categories</p>
+            <div className="">
+                <div className="">
+                    <img src={assets.box_icon} alt="icon" />
+                    <p className="">Fruit</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Categories;

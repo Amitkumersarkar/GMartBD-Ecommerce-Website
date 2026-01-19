955ff713f9b1b9d281024f4073b50049852c1d4f
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-20 01:02:50 +0600

    added features

--- src/components/BottomBanner/BottomBanner.jsx
+++ src/components/BottomBanner/BottomBanner.jsx
@@ -1,4 +1,4 @@
-import { assets } from "../../assets/assets";
+import { assets, features } from "../../assets/assets";
 
 const BottomBanner = () => {
 
@@ -6,6 +6,19 @@ const BottomBanner = () => {
         <div className="relative mt-24">
             <img className="w-full hidden md:block" src={assets.bottom_banner_image} alt="image" />
             <img className="w-full md:hidden" src={assets.bottom_banner_image_sm} alt="image" />
+
+            <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
+                <div className="">
+                    <h1 className="text-2xl md:text-3xl font-semibold text-pink-600 mb-6">Why We Are the Best?</h1>
+                    {features.map((feature, idx) => (
+                        <div key={idx} className="flex items-center gap-4 mt-2">
+                            <img className="md:max-w-11 w-9" src={feature.icon} alt={feature.title} />
+                            <h3 className="text-lg md:text-xl font-semibold">{feature.title}</h3>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };

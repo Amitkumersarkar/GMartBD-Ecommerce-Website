2e02fa522603009ad3f1c88e71a4ad0fc04d65ba
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-20 00:26:07 +0600

    added bottom banner section

---
+++ src/components/BottomBanner/BottomBanner.jsx
@@ -0,0 +1,13 @@
+import { assets } from "../../assets/assets";
+
+const BottomBanner = () => {
+
+    return (
+        <div className="relative mt-24">
+            <img className="w-full hidden md:block" src={assets.bottom_banner_image} alt="image" />
+            <img className="w-full md:hidden" src={assets.bottom_banner_image_sm} alt="image" />
+        </div>
+    );
+};
+
+export default BottomBanner;

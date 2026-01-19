dbb19b1722bbbc1c9c658c1daf7e546460b70ec4
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-19 21:14:23 +0600

    fixes error

--- src/pages/BestSeller/BestSeller.jsx
+++ src/pages/BestSeller/BestSeller.jsx
@@ -4,16 +4,21 @@ import ProductCard from "../ProductCard/ProductCard";
 const BestSeller = () => {
     const { products } = useAuthContext();
 
-    return (
-        <div className="mt-16">
-            <p className="text-2xl text-gray-700 md:text-3xl font-medium">Best Sellers</p>
-            <div className="">
-                {
-                    products.filter((product) => product.inStock).slice(0, 5).map((product, idx) => (
+    if (!products) return null;
 
+    return (
+        <div className="mt-16 px-4 sm:px-6 md:px-0">
+            <p className="text-2xl text-gray-700 md:text-3xl font-medium">
+                Best Sellers
+            </p>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6">
+                {products
+                    .filter((product) => product.inStock)
+                    .slice(0, 5)
+                    .map((product, idx) => (
                         <ProductCard key={idx} product={product}></ProductCard>
-                    ))
-                }
+                    ))}
             </div>
         </div>
     );

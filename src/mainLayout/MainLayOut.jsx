61f99e08a808c8fb5fc550d87b44286db108718d
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-03 23:37:27 +0600

    user login context added

--- src/mainLayout/MainLayOut.jsx
+++ src/mainLayout/MainLayOut.jsx
@@ -3,14 +3,18 @@ import Navbar from "../components/Navbar/Navbar";
 import Home from "../components/HomePage/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "../components/Footer/Footer";
+import { useAuthContext } from "../context/AuthContext";
+import SignIn from "../auth/SignIn";
 
 const MainLayOut = () => {
     const { pathname } = useLocation();
     const isSellerPath = pathname.includes("seller");
+    const { showUserLogin } = useAuthContext();
 
     return (
         <div>
             {isSellerPath ? null : <Navbar />}
+            {showUserLogin ? <SignIn></SignIn> : null}
             <Toaster></Toaster>
             <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
                 <Routes>

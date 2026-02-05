c8132d5f76b90b4c4ac2d08296d7571cb92049b9
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-05 21:05:17 +0600

    added context apis and filter and find methods

--- src/pages/ProductCategory/ProductCategory.jsx
+++ src/pages/ProductCategory/ProductCategory.jsx
@@ -1,13 +1,22 @@
 import { useParams } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
+import { categories } from "../../assets/assets";
 
 const ProductCategory = () => {
     const { products } = useAuthContext();
     const { category } = useParams();
+    const searchCategory = categories.find((item) => item.path.toLocaleLowerCase() === category)
+    const filteredProducts = products.filter((product) => product.category.toLocaleLowerCase() === category)
 
     return (
-        <div>
-
+        <div className="">
+            {
+                searchCategory && (
+                    <div className="">
+                        <p className=""></p>
+                    </div>
+                )
+            }
         </div>
     );
 };

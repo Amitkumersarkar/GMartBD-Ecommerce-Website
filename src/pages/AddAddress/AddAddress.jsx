286f1fb89c6f8a7727dc5c2a13362af0f7402bbc
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-12 19:25:54 +0600

    ui style

--- src/pages/AddAddress/AddAddress.jsx
+++ src/pages/AddAddress/AddAddress.jsx
@@ -1,3 +1,5 @@
+import { assets } from "../../assets/assets";
+
 // input field components
 const inputField = (() => (
     <input type="text"></input>
@@ -6,7 +8,13 @@ const AddAddress = () => {
 
     return (
         <div className="mt-16 pb-16">
-            <p className="text-2xl md:text-3xl text-gray-500">Add Shipping<span className="font-semibold text-primary">Address</span></p>
+            <p className="text-2xl md:text-3xl text-gray-500">Add Shipping<span className="font-semibold text-pink-600"> Address </span></p>
+            <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+                <div className="flex-1 max-w-md">
+
+                </div>
+                <img className="md:mr-16 mb-16 md:mt-0" src={assets.add_address_iamge} alt="address" />
+            </div>
         </div>
     );
 };

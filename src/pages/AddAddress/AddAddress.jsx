37de3aad67d5ea2dd7e6d6a8c295298b04e5763a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-12 19:42:18 +0600

    added input field and from page

--- src/pages/AddAddress/AddAddress.jsx
+++ src/pages/AddAddress/AddAddress.jsx
@@ -1,8 +1,15 @@
 import { assets } from "../../assets/assets";
 
 // input field components
-const inputField = (() => (
-    <input type="text"></input>
+const inputField = ((type, placeholder, name, handleChange, address) => (
+    <input className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-pink-600 transition"
+        type={type}
+        placeholder={placeholder}
+        onChange={handleChange}
+        name={name}
+        value={address[name]}
+        required
+    ></input>
 ))
 const AddAddress = () => {
     const handleSubmit = async (e) => {
@@ -14,9 +21,9 @@ const AddAddress = () => {
             <p className="text-2xl md:text-3xl text-gray-500">Add Shipping<span className="font-semibold text-pink-600"> Address </span></p>
             <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
                 <div className="flex-1 max-w-md">
-                    <form onSubmit={handleSubmit} className="space-y-3 mt-6 text-sm">
+                    <form onSubmit={handleSubmit} address={address} name="firstName" type="text" placeholder="Fist Name" className="space-y-3 mt-6 text-sm">
                         <div className="">
-
+                            <inputField handleChange={handleChange}></inputField>
                         </div>
                     </form>
                 </div>

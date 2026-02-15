cfb20e768951a7ae7a9d1daa279f9d16101f25a3
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-16 00:45:44 +0600

    added seller navigation path

--- src/components/SellerDashBoard/SellerLogIn.jsx
+++ src/components/SellerDashBoard/SellerLogIn.jsx
@@ -1,9 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
+import { useEffect, useState } from "react";
 
 const SellerLogIn = () => {
     const { isSeller, setIsSeller } = useAuthContext();
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, SetPassword] = useState("");
+
+    useEffect(() => {
+        if (isSeller) {
+            navigate("/seller");
+        }
+    }, [isSeller]);
 
     return (
         <div>

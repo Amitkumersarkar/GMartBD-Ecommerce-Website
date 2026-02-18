b92108d08693273928997dceb9664133276551d0
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-19 01:26:48 +0600

    changes css style

--- src/pages/Seller/SellerLayOut.jsx
+++ src/pages/Seller/SellerLayOut.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { useAuthContext } from "../../context/AuthContext";
 
@@ -19,7 +19,7 @@ const SellerLayOut = () => {
     return (
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white ">
-                <Link href="/">
+                <Link to="/">
                     <img className="cursor-pointer w-34 md:w-38" src={assets.logo} alt="logo" />
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
@@ -27,19 +27,21 @@ const SellerLayOut = () => {
                     <button onClick={logOut} className='border rounded-full text-sm px-4 py-1'>Logout</button>
                 </div>
             </div>
-            <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
-                {sidebarLinks.map((item, index) => (
-                    <a href={item.path} key={index}
-                        className={`flex items-center py-3 px-4 gap-3 
-                            ${index === 0 ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
-                                : "hover:bg-gray-100/90 border-white text-gray-700"
-                            }`
-                        }
-                    >
-                        {item.icon}
-                        <p className="md:block hidden text-center">{item.name}</p>
-                    </a>
-                ))}
+            <div className="flex ">
+                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                    {sidebarLinks.map((item, index) => (
+                        <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
+                            className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
+                            ${isActive ? "border-r-4 md:border-r-[6px] bg-pink-600/10 border-pink-600 text-pink-600"
+                                    : "hover:bg-gray-100/90 border-white"
+                                }`
+                            }
+                        >
+                            {item.icon}
+                            <p className="md:block hidden text-center">{item.name}</p>
+                        </NavLink>
+                    ))}
+                </div>
             </div>
         </>
     );

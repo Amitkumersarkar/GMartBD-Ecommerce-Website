73383b3e66f6133eea9e160ca844141401589873
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-02-22 17:12:00 +0600

    event handling functions added

--- src/pages/Seller/AddProduct/AddProduct.jsx
+++ src/pages/Seller/AddProduct/AddProduct.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { assets } from "../../../assets/assets";
+import { assets, categories } from "../../../assets/assets";
 
 const AddProduct = () => {
     const [files, setFiles] = useState([]);
@@ -47,24 +47,26 @@ const AddProduct = () => {
                 </div>
                 <div className="w-full flex flex-col gap-1">
                     <label className="text-base font-medium" htmlFor="category">Category</label>
-                    <select id="category" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40">
+                    <select onChange={(e) => setCategory(e.target.value)} value={category} id="category" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40">
                         <option value="">Select Category</option>
-                        {[{ name: 'Electronics' }, { name: 'Clothing' }, { name: 'Accessories' }].map((item, index) => (
-                            <option key={index} value={item.name}>{item.name}</option>
-                        ))}
+                        {
+                            categories.map((item, index) => (
+                                <option key={index} value={item.path}> {item.path}</option>
+                            ))
+                        }
                     </select>
                 </div>
                 <div className="flex items-center gap-5 flex-wrap">
                     <div className="flex-1 flex flex-col gap-1 w-32">
                         <label className="text-base font-medium" htmlFor="product-price">Product Price</label>
-                        <input id="product-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
+                        <input onChange={(e) => setPrice(e.target.value)} value={price} id="product-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                     <div className="flex-1 flex flex-col gap-1 w-32">
                         <label className="text-base font-medium" htmlFor="offer-price">Offer Price</label>
-                        <input id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
+                        <input onChange={(e) => setOfferPrice(e.target.value)} value={offerPrice} id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                 </div>
-                <button className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded">ADD</button>
+                <button className="px-8 py-2.5 bg-pink-600 text-white font-medium rounded">ADD</button>
             </form>
         </div>
     );
